09eeafd65c1cbc8d19448489043939cb8bf5c3cb
Werapat <94524977+hellp002@users.noreply.github.com>
2022-05-05 22:25:15 +0700

    Create Vector2D.java

---
+++ src/tools/Vector2D.java
@@ -0,0 +1,74 @@
+package tools;
+
+
+public class Vector2D {
+	private double x,y;
+	
+	public Vector2D() {
+		this.x = 0;
+		this.y = 0;
+	}
+	
+	public Vector2D(double x,double y) {
+		this.x = x;
+		this.y = y;
+	}
+	
+	public Vector2D(Vector2D copyOne) {
+		this.x = copyOne.x;
+		this.y = copyOne.y;
+	}
+	
+	public double getMagnitude() {
+		return Math.hypot(x, y);
+	}
+	
+	public Vector2D getUnitVector() {
+		if (this.getMagnitude() == 0) {
+			return new Vector2D();
+		}
+		return new Vector2D((this.x/this.getMagnitude()),(this.y/this.getMagnitude()));
+	}
+	
+	public Vector2D getUnitVectorMulConst(int cons) {
+		Vector2D copyOne = getUnitVector();
+		copyOne.setX(copyOne.x * cons);
+		copyOne.setY(copyOne.y * cons);
+		return copyOne;
+	}
+	
+	public void setRotatingVector(double degree) {
+		double newX = this.x * Math.cos(Math.toRadians(degree)) - this.y * Math.sin(Math.toRadians(degree));
+		double newY = this.x * Math.sin(Math.toRadians(degree)) + this.y * Math.cos(Math.toRadians(degree));
+		this.setX(newX);
+		this.setY(newY);
+		
+	}
+	
+	public void addVector(Vector2D other) {
+		setX(x + other.getX());
+		setY(y + other.getY());
+	}
+
+	public double getX() {
+		return x;
+	}
+
+	public void setX(double x) {
+		this.x = x;
+	}
+
+	public double getY() {
+		return y;
+	}
+
+	public void setY(double y) {
+		this.y = y;
+	}
+	public void reset() {
+		setX(0);
+		setY(0);
+	}
+	
+	
+}
