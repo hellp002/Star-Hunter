ee7ff4c40a3957098c761e354df3ae9523fb84f3
Werapat <94524977+hellp002@users.noreply.github.com>
2022-05-06 03:47:55 +0700

    Add selection menu

--- src/gui/SelectedType.java
+++ src/gui/SelectedType.java
@@ -1,12 +1,77 @@
 package gui;
 
+import bgm.AudioLoad;
+import bgm.SFXPlayer;
+import javafx.geometry.HPos;
+import javafx.geometry.Insets;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundFill;
+import javafx.scene.layout.CornerRadii;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.StackPane;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Text;
+import logic.Player;
+import logic.SceneController;
+import tools.Data;
 
-public class SelectedType extends GridPane{
-	private int health,power,armour;
-	private double speed,shootSpeed,attackSpeed;
-	
-	public SelectedType(int health, int power, double speed, double shootSpeed, double attackSpeed, int armour) {
+public class SelectedType extends GridPane {
+	private int health, power, armour;
+	private double speed, shootSpeed, attackSpeed;
+	private Text[] info = new Text[6];
+	private final static int PADDING = 10;
+
+	public SelectedType(int health, int power, double speed, double shootSpeed, double attackSpeed, int armour,String text) {
+		this.health = health;
+		this.power = power;
+		this.speed = speed;
+		this.armour = armour;
+		this.shootSpeed = shootSpeed;
+		this.attackSpeed = attackSpeed;
 		
+		info[0] = new Text("Health : " + this.health);
+		info[1] = new Text("Attack : " + this.power);
+		info[2] = new Text("Speed : " + this.speed);
+		info[3] = new Text("Shot Speed : " + this.shootSpeed);
+		info[4] = new Text("Attack Speed : " + this.attackSpeed);
+		info[5] = new Text("Armour : " + this.armour);
+		for (int i = 0; i < info.length; i++) {
+			info[i].setFill(Color.ORANGE);
+			info[i].setFont(Data.FONT16);
+			this.add(info[i], 0, i+1);
+		}
+		Text type = new Text("Type : " + text);
+		
+		this.add(type, 0, 0);
+		GridPane.setHalignment(type, HPos.CENTER);
+		this.setPadding(new Insets(PADDING));
+		this.setHgap(PADDING);
+		this.setVgap(PADDING);
+		type.setFont(Data.FONT16);
+		this.setOnMouseClicked((MouseEvent Event) ->{
+			onClickHandle();
+		});
+		type.setFill(Color.ORANGE);
+		this.setBackground(new Background(new BackgroundFill(Color.BLUE, CornerRadii.EMPTY, Insets.EMPTY)));
+		this.setOnMouseEntered((MouseEvent Event) -> {
+			this.setBackground(new Background(new BackgroundFill(Color.RED, CornerRadii.EMPTY, Insets.EMPTY)));
+			SFXPlayer.getSfxMap().get("enter").play();
+		});
+		this.setOnMouseExited((MouseEvent Event) -> {
+			this.setBackground(new Background(new BackgroundFill(Color.BLUE, CornerRadii.EMPTY, Insets.EMPTY)));
+		});
+		
+
+	}
+	
+	private void onClickHandle() {
+		Player.newPlayer(health, power, speed, shootSpeed, attackSpeed, armour);
+		StackPane a = SceneController.getControlMap().get("Main_Menu").getPane();
+		a.getChildren().clear();
+		SceneController.getControlMap().get("Main_Menu").getButtonZone().setDisable(false);
+		SceneController.loadGame();
+		AudioLoad.playMusic("Dungeon");
+		SFXPlayer.getSfxMap().get("btn").play();
 	}
 }
--- src/gui/Selection.java
+++ src/gui/Selection.java
@@ -1,11 +1,32 @@
 package gui;
 
-import javafx.scene.layout.HBox;
+import javafx.geometry.HPos;
+import javafx.geometry.Insets;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundFill;
+import javafx.scene.layout.CornerRadii;
+import javafx.scene.layout.GridPane;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Text;
+import tools.Data;
 
-public class Selection extends HBox {
+public class Selection extends GridPane {
 	
 	public Selection() {
-		
+		SelectedType choice1 = new SelectedType(100,15,3,5.5,1.3,15,"Balancer");
+		SelectedType choice2 = new SelectedType(200,12,2.8,5,1.4,20,"Tank");
+		SelectedType choice3 = new SelectedType(50,18,3.8,6,1.2,10,"Speedy");
+		this.add(choice1, 0, 1);
+		this.add(choice2, 1, 1);
+		this.add(choice3, 2, 1);
+		Text text = new Text("Choose your type");
+		text.setFont(Data.FONT16);
+		text.setFill(Color.ORANGE);
+		this.add(text, 0, 0);
+		this.setPadding(new Insets(10));
+		this.setBackground(new Background(new BackgroundFill(Color.BLUE, CornerRadii.EMPTY, Insets.EMPTY)));
+		GridPane.setColumnSpan(text,3);
+		GridPane.setHalignment(text,HPos.CENTER);
 	}
 	
 	
